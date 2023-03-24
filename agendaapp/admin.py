9122c38afcd1a7721d83b117d25ca7bd4c7f6bad
dev <rogerio.pereira@ifto.edu.br>
2023-03-24 09:27:55 -0300

    admin

--- agendaapp/admin.py
+++ agendaapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from agendaapp.models import Servico,Agenda
 
 # Register your models here.
+admin.site.register(Servico)
+admin.site.register(Agenda)
--- agendaapp/models.py
+++ agendaapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Servico(models.Model):
+    nome_servico =models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nome_servico
+
+
+class Agenda(models.Model):
+    data_agenda =models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.data_agenda
