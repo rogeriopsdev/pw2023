8e1e746aeeb124f4a2a20dd01a995190af59028d
dev <rogerio.pereira@ifto.edu.br>
2023-06-20 10:01:08 -0300

    Versao4

--- agendaapp/views.py
+++ agendaapp/views.py
@@ -7,4 +7,10 @@ def index(request):
 
 def cad_servico(request):
     form = Servicoform()
+    if request.method=="POST":
+        form = Servicoform(request.POST, request.FILES)
+        if form.is_valid():
+            obj = form.save()
+            obj.save()
+            form = Servicoform()
     return render(request, 'agenda/criar_serv.html',{'form':form})
